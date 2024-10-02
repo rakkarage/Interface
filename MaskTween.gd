aeca5e911c701e634ad85569b4fa95546ade38b4
Rakka Rage <rakkarage@gmail.com>
2024-10-02 11:11:56 -0400

    x

--- MaskTween.gd
+++ MaskTween.gd
@@ -13,7 +13,7 @@ func animate_in() -> void:
 	var scale_current = _mask.material.get_shader_parameter("scale")
 	var color_current = _mask.material.get_shader_parameter("color")
 	var tween := create_tween()
-	tween.tween_callback(func(): _mask.mouse_filter = Control.MOUSE_FILTER_STOP)
+	tween.tween_callback(func() -> void: _mask.mouse_filter = Control.MOUSE_FILTER_STOP)
 	tween.tween_method(_set_shader_scale, scale_current, _scale_max, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
 	tween.parallel().tween_method(_set_shader_color, color_current, _color_max, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 	await tween.finished
@@ -24,7 +24,7 @@ func animate_out() -> void:
 	var tween := create_tween()
 	tween.tween_method(_set_shader_scale, scale_current, _scale_min, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 	tween.parallel().tween_method(_set_shader_color, color_current, _color_min, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
-	tween.tween_callback(func(): _mask.mouse_filter = Control.MOUSE_FILTER_IGNORE)
+	tween.tween_callback(func() -> void: _mask.mouse_filter = Control.MOUSE_FILTER_IGNORE)
 	await tween.finished
 
 func _set_shader_color(newColor: Color) -> void:
