6cc6f2fc967f65b1343ad4f7b98c1e51a209bc53
Rakka Rage <rakkarage@gmail.com>
2024-10-05 18:00:05 -0400

    x

--- MaskTween.gd
+++ MaskTween.gd
@@ -3,27 +3,27 @@ class_name MaskTween
 
 @onready var _mask: ColorRect = $Mask
 
-@export var _duration = 1.0
-@export var _scale_min = 0.0
-@export var _scale_max = 1000.0
-@export var _color_min = Color(0, 0, 0, 0)
-@export var _color_max = Color(0, 0, 0, 1)
+@export var duration = 1.0
+@export var scale_min = 0.0
+@export var scale_max = 1000.0
+@export var color_min = Color(0, 0, 0, 0)
+@export var color_max = Color(0, 0, 0, 1)
 
 func animate_in() -> void:
 	var scale_current = _mask.material.get_shader_parameter("scale")
 	var color_current = _mask.material.get_shader_parameter("color")
 	var tween := create_tween()
 	tween.tween_callback(func() -> void: _mask.mouse_filter = Control.MOUSE_FILTER_STOP)
-	tween.tween_method(_set_shader_scale, scale_current, _scale_max, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
-	tween.parallel().tween_method(_set_shader_color, color_current, _color_max, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.tween_method(_set_shader_scale, scale_current, scale_max, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
+	tween.parallel().tween_method(_set_shader_color, color_current, color_max, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 	await tween.finished
 
 func animate_out() -> void:
 	var scale_current = _mask.material.get_shader_parameter("scale")
 	var color_current = _mask.material.get_shader_parameter("color")
 	var tween := create_tween()
-	tween.tween_method(_set_shader_scale, scale_current, _scale_min, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
-	tween.parallel().tween_method(_set_shader_color, color_current, _color_min, _duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
+	tween.tween_method(_set_shader_scale, scale_current, scale_min, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
+	tween.parallel().tween_method(_set_shader_color, color_current, color_min, duration).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
 	tween.tween_callback(func() -> void: _mask.mouse_filter = Control.MOUSE_FILTER_IGNORE)
 	await tween.finished
 
